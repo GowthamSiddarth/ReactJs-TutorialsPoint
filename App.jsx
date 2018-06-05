70be7b4ce6c58c4540a36edebcd4ade3057f8bc7
Gowtham Behara <gothsiddu@gmail.com>
2018-06-05 15:09:29 +0530

    Render table as component

--- App.jsx
+++ App.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            data: [
+                {
+                    'id': 1,
+                    'name': 'Foo',
+                    'age': 23
+                },
+                {
+                    'id': 2,
+                    'name': 'Bar',
+                    'age': 24
+                },
+                {
+                    'id': 3,
+                    'name': 'Etc',
+                    'age': 22
+                }
+            ]
+        }
+    }
     render() {
         return (
             <div>
                 <div>Hello World!</div>
                 <div><Header /></div>
-                <div><Content /></div>
+                <div><Table data={this.state.data} /></div>
             </div>
         );
     }
@@ -20,11 +42,27 @@ class Header extends React.Component {
     }
 }
 
-class Content extends React.Component {
+class Table extends React.Component {
     render() {
         return (
-            <div>Content</div>
+            <table>
+                <tbody>
+                    {this.props.data.map((person, i) => <TableRow key={i} data={person} />)}
+                </tbody>
+            </table>
         )
+    }
+}
+
+class TableRow extends React.Component {
+    render() {
+        return (
+            <tr>
+                <td>{this.props.data.id}</td>
+                <td>{this.props.data.name}</td>
+                <td>{this.props.data.age}</td>
+            </tr>
+        );
     }
 }
 
