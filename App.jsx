ccde9de4f35895847f11fb32a2e65f81fa0ae1c4
Gowtham Behara <gothsiddu@gmail.com>
2018-06-05 14:27:50 +0530

    Add Header & Content components

--- App.jsx
+++ App.jsx
@@ -3,8 +3,28 @@ import React from 'react';
 class App extends React.Component {
     render() {
         return (
-            <div>Hello World!</div>                
+            <div>
+                <div>Hello World!</div>
+                <div><Header /></div>
+                <div><Content /></div>
+            </div>
         );
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>Header</div>
+        )
+    }
+}
+
+class Content extends React.Component {
+    render() {
+        return (
+            <div>Content</div>
+        )
     }
 }
 
